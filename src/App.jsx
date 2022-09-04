d4d21b4653240404d77076de84e88625e0f96a8a
Manuel Ortiz <70727928+hctmanuelortiz@users.noreply.github.com>
2022-09-04 18:47:53 -0300

    Revert "Update App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ const App = () => {
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
               <Marker
                 
